3e27c12214f7a0ff37d5eb6409c6da95aae2fdb7
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2024-09-27 15:25:18 -0500

    Update 9/27

--- Lang54c/Lang54c/MainForm.py
+++ Lang54c/Lang54c/MainForm.py
@@ -14,11 +14,8 @@ class MainForm(Form):
 		self._textBox1 = System.Windows.Forms.TextBox()
 		self._button2 = System.Windows.Forms.Button()
 		self._button3 = System.Windows.Forms.Button()
-		self._textBox2 = System.Windows.Forms.TextBox()
-		self._textBox3 = System.Windows.Forms.TextBox()
 		self._label3 = System.Windows.Forms.Label()
 		self._label4 = System.Windows.Forms.Label()
-		self._textBox4 = System.Windows.Forms.TextBox()
 		self._label2 = System.Windows.Forms.Label()
 		self._label5 = System.Windows.Forms.Label()
 		self.SuspendLayout()
@@ -39,16 +36,16 @@ class MainForm(Form):
 		# 
 		self._label1.BackColor = System.Drawing.SystemColors.ButtonHighlight
 		self._label1.Font = System.Drawing.Font("Modern No. 20", 17.9999981, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._label1.Location = System.Drawing.Point(293, 95)
+		self._label1.Location = System.Drawing.Point(268, 203)
 		self._label1.Name = "label1"
 		self._label1.Size = System.Drawing.Size(106, 30)
 		self._label1.TabIndex = 1
-		self._label1.Text = "label1"
+		self._label1.Text = "Radius"
 		# 
 		# textBox1
 		# 
 		self._textBox1.Font = System.Drawing.Font("Modern No. 20", 15.749999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._textBox1.Location = System.Drawing.Point(493, 95)
+		self._textBox1.Location = System.Drawing.Point(493, 203)
 		self._textBox1.Name = "textBox1"
 		self._textBox1.Size = System.Drawing.Size(209, 30)
 		self._textBox1.TabIndex = 2
@@ -77,53 +74,29 @@ class MainForm(Form):
 		self._button3.UseVisualStyleBackColor = False
 		self._button3.Click += self.Button3Click
 		# 
-		# textBox2
-		# 
-		self._textBox2.Font = System.Drawing.Font("Modern No. 20", 15.749999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._textBox2.Location = System.Drawing.Point(493, 131)
-		self._textBox2.Name = "textBox2"
-		self._textBox2.Size = System.Drawing.Size(209, 30)
-		self._textBox2.TabIndex = 5
-		# 
-		# textBox3
-		# 
-		self._textBox3.Font = System.Drawing.Font("Modern No. 20", 15.749999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._textBox3.Location = System.Drawing.Point(493, 167)
-		self._textBox3.Name = "textBox3"
-		self._textBox3.Size = System.Drawing.Size(209, 30)
-		self._textBox3.TabIndex = 6
-		# 
 		# label3
 		# 
 		self._label3.BackColor = System.Drawing.SystemColors.ButtonHighlight
 		self._label3.Font = System.Drawing.Font("Modern No. 20", 17.9999981, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._label3.Location = System.Drawing.Point(293, 236)
+		self._label3.Location = System.Drawing.Point(268, 236)
 		self._label3.Name = "label3"
 		self._label3.Size = System.Drawing.Size(106, 30)
 		self._label3.TabIndex = 10
-		self._label3.Text = "label3"
+		self._label3.Text = "Area"
 		# 
 		# label4
 		# 
 		self._label4.BackColor = System.Drawing.SystemColors.ButtonHighlight
 		self._label4.Font = System.Drawing.Font("Modern No. 20", 17.9999981, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._label4.Location = System.Drawing.Point(293, 271)
+		self._label4.Location = System.Drawing.Point(268, 271)
 		self._label4.Name = "label4"
-		self._label4.Size = System.Drawing.Size(106, 30)
+		self._label4.Size = System.Drawing.Size(157, 30)
 		self._label4.TabIndex = 11
-		self._label4.Text = "label4"
-		# 
-		# textBox4
-		# 
-		self._textBox4.Font = System.Drawing.Font("Modern No. 20", 15.749999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._textBox4.Location = System.Drawing.Point(493, 203)
-		self._textBox4.Name = "textBox4"
-		self._textBox4.Size = System.Drawing.Size(209, 30)
-		self._textBox4.TabIndex = 12
+		self._label4.Text = "Circumference"
 		# 
 		# label2
 		# 
-		self._label2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		self._label2.BackColor = System.Drawing.SystemColors.ControlDark
 		self._label2.Font = System.Drawing.Font("Modern No. 20", 15.749999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self._label2.Location = System.Drawing.Point(493, 236)
 		self._label2.Name = "label2"
@@ -132,7 +105,7 @@ class MainForm(Form):
 		# 
 		# label5
 		# 
-		self._label5.BackColor = System.Drawing.SystemColors.ButtonHighlight
+		self._label5.BackColor = System.Drawing.SystemColors.ControlDark
 		self._label5.Font = System.Drawing.Font("Modern No. 20", 15.749999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self._label5.Location = System.Drawing.Point(493, 272)
 		self._label5.Name = "label5"
@@ -145,11 +118,8 @@ class MainForm(Form):
 		self.ClientSize = System.Drawing.Size(947, 442)
 		self.Controls.Add(self._label5)
 		self.Controls.Add(self._label2)
-		self.Controls.Add(self._textBox4)
 		self.Controls.Add(self._label4)
 		self.Controls.Add(self._label3)
-		self.Controls.Add(self._textBox3)
-		self.Controls.Add(self._textBox2)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._textBox1)
@@ -157,7 +127,6 @@ class MainForm(Form):
 		self.Controls.Add(self._button1)
 		self.Name = "MainForm"
 		self.Text = "Lang54c"
-		self.Load += self.MainFormLoad
 		self.ResumeLayout(False)
 		self.PerformLayout()
 
@@ -166,9 +135,11 @@ class MainForm(Form):
 		pi = 3.14159
 		radius = float(self._textBox1.Text)
 		area = pi * radius ** 2
-		self._label2.Text = str(area)
+		aarea = round(area, 3)
+		circum = 2 * radius
+		self._label2.Text = str(aarea)
+		self._label5.Text = str(circum)
 		
-		# To be continued !!!!!!!!!!!!!!!!!!!!!!
 		
 		# + - * / % 	** pow	// divide & round down
 		#int (Integer): Whole number, positive or negative
@@ -178,9 +149,6 @@ class MainForm(Form):
 
 	def Button2Click(self, sender, e):
 		self._textBox1.Text = ""
-		self._textBox2.Text = ""
-		self._textBox3.Text = ""
-		self._textBox4.Text = ""
 		self._label2.Text = ""
 		self._label5.Text = ""
 
