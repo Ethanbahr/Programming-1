ad4b356ed7b2f88b760507c90206ae3a20853744
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2024-10-23 15:26:53 -0500

    Update 10/23

---
+++ LP4-2/LP4-2/Program.py
@@ -0,0 +1,15 @@
+﻿#to be completed
+plwt = int (input("Enter package weight in kilograms: "))
+pl 	 = int (input("Enter package length in centimeters: "))
+pw 	 = int (input("Enter package width in centimeters: "))
+ph 	 = int (input("Enter package height in centimeters: "))
+pv 	 = pl * pw * ph
+
+if plwt < 27 and pv < 100000:
+	print("Package too heavy and too large")
+elif plwt < 27:
+	print("Package too heavy")
+elif pv < 100000:
+	print("Package too large")
+input()	#Press 'Enter' to close; only needed for SharpDevelop
+	
