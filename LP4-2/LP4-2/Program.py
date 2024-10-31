52c558a178b9c8d43927f7db66ac2258c39ed941
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2024-10-31 15:28:11 -0500

    Update 10/31

--- LP4-2/LP4-2/Program.py
+++ LP4-2/LP4-2/Program.py
@@ -5,11 +5,7 @@ pw 	 = int (input("Enter package width in centimeters: "))
 ph 	 = int (input("Enter package height in centimeters: "))
 pv 	 = pl * pw * ph
 
-if plwt < 27 and pv < 100000:
-	print("Package too heavy and too large")
-elif plwt < 27:
-	print("Package too heavy")
-elif pv < 100000:
-	print("Package too large")
+
+
 input()	#Press 'Enter' to close; only needed for SharpDevelop
 	
--- LP4-3/LP4-3/Program.py
+++ LP4-3/LP4-3/Program.py
@@ -1,1 +1,17 @@
-﻿# To be completed
+﻿doe = int (input("Enter how many dozens of eggs: "))
+ppd = 0
+tp 	= 0
+remainder = doe % 12
+if doe > 0 and doe < 4:
+	ppd = 0.50
+elif doe >= 4 and doe < 6:
+	ppd = 0.45
+elif doe >= 6 and doe < 11:
+	ppd = 0.40
+elif doe >= 11:
+	ppd = 0.35
+
+tp = doe * ppd + (remainder * 1/0.5)
+print("Price per dozen:" + str(ppd))
+print("Total price is:" + str(tp))
+input()
