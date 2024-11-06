d3fd02dd86329af54fe28a97f8af06b482da2cf2
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2024-11-06 15:27:38 -0600

    Update 11/6

--- Prog122d/Prog122d/MainForm.py
+++ Prog122d/Prog122d/MainForm.py
@@ -17,11 +17,12 @@ class MainForm(Form):
 		# 
 		# button1
 		# 
-		self._button1.BackColor = System.Drawing.Color.DarkRed
-		self._button1.ForeColor = System.Drawing.Color.White
+		self._button1.BackColor = System.Drawing.Color.DarkTurquoise
+		self._button1.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.ForeColor = System.Drawing.Color.Black
 		self._button1.Location = System.Drawing.Point(12, 330)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(96, 49)
+		self._button1.Size = System.Drawing.Size(102, 51)
 		self._button1.TabIndex = 0
 		self._button1.Text = "Calculate"
 		self._button1.UseVisualStyleBackColor = False
@@ -29,11 +30,11 @@ class MainForm(Form):
 		# 
 		# button2
 		# 
-		self._button2.BackColor = System.Drawing.Color.DarkRed
-		self._button2.ForeColor = System.Drawing.Color.White
-		self._button2.Location = System.Drawing.Point(114, 330)
+		self._button2.BackColor = System.Drawing.Color.DarkTurquoise
+		self._button2.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(120, 330)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(96, 49)
+		self._button2.Size = System.Drawing.Size(102, 51)
 		self._button2.TabIndex = 1
 		self._button2.Text = "Clear"
 		self._button2.UseVisualStyleBackColor = False
@@ -41,11 +42,11 @@ class MainForm(Form):
 		# 
 		# button3
 		# 
-		self._button3.BackColor = System.Drawing.Color.DarkRed
-		self._button3.ForeColor = System.Drawing.Color.White
-		self._button3.Location = System.Drawing.Point(216, 330)
+		self._button3.BackColor = System.Drawing.Color.DarkTurquoise
+		self._button3.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(228, 330)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(96, 49)
+		self._button3.Size = System.Drawing.Size(102, 51)
 		self._button3.TabIndex = 2
 		self._button3.Text = "Exit"
 		self._button3.UseVisualStyleBackColor = False
@@ -53,36 +54,36 @@ class MainForm(Form):
 		# 
 		# listBox1
 		# 
-		self._listBox1.BackColor = System.Drawing.Color.White
+		self._listBox1.BackColor = System.Drawing.Color.AliceBlue
+		self._listBox1.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self._listBox1.FormattingEnabled = True
+		self._listBox1.ItemHeight = 20
 		self._listBox1.Location = System.Drawing.Point(12, 12)
 		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(300, 316)
+		self._listBox1.Size = System.Drawing.Size(318, 304)
 		self._listBox1.TabIndex = 3
 		# 
 		# MainForm
 		# 
-		self.BackColor = System.Drawing.Color.Firebrick
-		self.ClientSize = System.Drawing.Size(324, 391)
+		self.BackColor = System.Drawing.Color.SkyBlue
+		self.ClientSize = System.Drawing.Size(342, 393)
 		self.Controls.Add(self._listBox1)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
 		self.Name = "MainForm"
-		self.Text = "Prog122b"
+		self.Text = "Prog122d"
 		self.ResumeLayout(False)
 
 
 	def Button1Click(self, sender, e):
-		header = "Hours:\t\tPay:"
+		header = "x" + "\t\t" + "Y"
 		self._listBox1.Items.Add(header)
-		for hour in range(0, 41):
-			pay = hour * 4
-			line = str(hour) + "\t\t" + str(int(pay))
-			self._listBox1.Items.Add(line)
+		for num in range()
+		# To be completed!!!!
 
 	def Button2Click(self, sender, e):
 		self._listBox1.Items.Clear()
 
 	def Button3Click(self, sender, e):
-		Application.Exit()
+		application.Exit()
