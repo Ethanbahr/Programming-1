f6ffe4588b4be843738d6def7bed3247f4b8d5a2
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2024-11-07 15:25:14 -0600

    Update 11/7

--- Prog122i/Prog122i/MainForm.py
+++ Prog122i/Prog122i/MainForm.py
@@ -17,12 +17,11 @@ class MainForm(Form):
 		# 
 		# button1
 		# 
-		self._button1.BackColor = System.Drawing.Color.DarkTurquoise
-		self._button1.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button1.ForeColor = System.Drawing.Color.Black
-		self._button1.Location = System.Drawing.Point(12, 330)
+		self._button1.BackColor = System.Drawing.Color.Cornsilk
+		self._button1.Font = System.Drawing.Font("Monotype Corsiva", 15.75, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(12, 335)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(102, 51)
+		self._button1.Size = System.Drawing.Size(116, 49)
 		self._button1.TabIndex = 0
 		self._button1.Text = "Calculate"
 		self._button1.UseVisualStyleBackColor = False
@@ -30,11 +29,11 @@ class MainForm(Form):
 		# 
 		# button2
 		# 
-		self._button2.BackColor = System.Drawing.Color.DarkTurquoise
-		self._button2.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button2.Location = System.Drawing.Point(120, 330)
+		self._button2.BackColor = System.Drawing.Color.Cornsilk
+		self._button2.Font = System.Drawing.Font("Monotype Corsiva", 15.75, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(134, 335)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(102, 51)
+		self._button2.Size = System.Drawing.Size(116, 49)
 		self._button2.TabIndex = 1
 		self._button2.Text = "Clear"
 		self._button2.UseVisualStyleBackColor = False
@@ -42,11 +41,11 @@ class MainForm(Form):
 		# 
 		# button3
 		# 
-		self._button3.BackColor = System.Drawing.Color.DarkTurquoise
-		self._button3.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button3.Location = System.Drawing.Point(228, 330)
+		self._button3.BackColor = System.Drawing.Color.Cornsilk
+		self._button3.Font = System.Drawing.Font("Monotype Corsiva", 15.75, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(256, 335)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(102, 51)
+		self._button3.Size = System.Drawing.Size(116, 49)
 		self._button3.TabIndex = 2
 		self._button3.Text = "Exit"
 		self._button3.UseVisualStyleBackColor = False
@@ -54,36 +53,37 @@ class MainForm(Form):
 		# 
 		# listBox1
 		# 
-		self._listBox1.BackColor = System.Drawing.Color.AliceBlue
-		self._listBox1.Font = System.Drawing.Font("MS Reference Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._listBox1.BackColor = System.Drawing.Color.Cornsilk
+		self._listBox1.Font = System.Drawing.Font("Monotype Corsiva", 15.75, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
 		self._listBox1.FormattingEnabled = True
-		self._listBox1.ItemHeight = 20
+		self._listBox1.ItemHeight = 25
 		self._listBox1.Location = System.Drawing.Point(12, 12)
 		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(318, 304)
+		self._listBox1.Size = System.Drawing.Size(360, 304)
 		self._listBox1.TabIndex = 3
 		# 
 		# MainForm
 		# 
-		self.BackColor = System.Drawing.Color.SkyBlue
-		self.ClientSize = System.Drawing.Size(342, 393)
+		self.BackColor = System.Drawing.Color.LightGreen
+		self.ClientSize = System.Drawing.Size(387, 396)
 		self.Controls.Add(self._listBox1)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
 		self.Controls.Add(self._button1)
 		self.Name = "MainForm"
-		self.Text = "Prog122d"
+		self.Text = "Prog122i"
 		self.ResumeLayout(False)
 
 
 	def Button1Click(self, sender, e):
-		header = "x" + "\t\t" + "Y"
+		header = "Num Cubed:" + "\t\t" + "Cube Root:"
 		self._listBox1.Items.Add(header)
-		for num in range(-12,17):
-			x = num
-			y = (int(x)**6) - (3 * int(x)**5) - (93 * int(x)**4) + (87 * int(x)**3) + (1596 * int(x)**2) - (1380 * int(x)) - 2800
-			line = str(x) + "\t\t" + str(y)
+		for num in range(-25,26):
+			nc 	= num**3
+			ncr = num**(1.0/3)
+			line = nc + "\t\t" + (ncr)
 			self._listBox1.Items.Add(line)
+			# Fix !!!!
 
 	def Button2Click(self, sender, e):
 		self._listBox1.Items.Clear()
