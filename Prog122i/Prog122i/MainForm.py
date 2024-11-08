61666aed04abb8979d8c847ac907a4c8e6fe5021
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2024-11-08 15:25:01 -0600

    Update 11/8

--- Prog122i/Prog122i/MainForm.py
+++ Prog122i/Prog122i/MainForm.py
@@ -19,9 +19,9 @@ class MainForm(Form):
 		# 
 		self._button1.BackColor = System.Drawing.Color.Cornsilk
 		self._button1.Font = System.Drawing.Font("Monotype Corsiva", 15.75, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
-		self._button1.Location = System.Drawing.Point(12, 335)
+		self._button1.Location = System.Drawing.Point(73, 335)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(116, 49)
+		self._button1.Size = System.Drawing.Size(95, 49)
 		self._button1.TabIndex = 0
 		self._button1.Text = "Calculate"
 		self._button1.UseVisualStyleBackColor = False
@@ -31,9 +31,9 @@ class MainForm(Form):
 		# 
 		self._button2.BackColor = System.Drawing.Color.Cornsilk
 		self._button2.Font = System.Drawing.Font("Monotype Corsiva", 15.75, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
-		self._button2.Location = System.Drawing.Point(134, 335)
+		self._button2.Location = System.Drawing.Point(198, 335)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(116, 49)
+		self._button2.Size = System.Drawing.Size(95, 49)
 		self._button2.TabIndex = 1
 		self._button2.Text = "Clear"
 		self._button2.UseVisualStyleBackColor = False
@@ -43,9 +43,9 @@ class MainForm(Form):
 		# 
 		self._button3.BackColor = System.Drawing.Color.Cornsilk
 		self._button3.Font = System.Drawing.Font("Monotype Corsiva", 15.75, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
-		self._button3.Location = System.Drawing.Point(256, 335)
+		self._button3.Location = System.Drawing.Point(320, 335)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(116, 49)
+		self._button3.Size = System.Drawing.Size(95, 49)
 		self._button3.TabIndex = 2
 		self._button3.Text = "Exit"
 		self._button3.UseVisualStyleBackColor = False
@@ -59,13 +59,13 @@ class MainForm(Form):
 		self._listBox1.ItemHeight = 25
 		self._listBox1.Location = System.Drawing.Point(12, 12)
 		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(360, 304)
+		self._listBox1.Size = System.Drawing.Size(467, 304)
 		self._listBox1.TabIndex = 3
 		# 
 		# MainForm
 		# 
 		self.BackColor = System.Drawing.Color.LightGreen
-		self.ClientSize = System.Drawing.Size(387, 396)
+		self.ClientSize = System.Drawing.Size(491, 396)
 		self.Controls.Add(self._listBox1)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
@@ -76,12 +76,12 @@ class MainForm(Form):
 
 
 	def Button1Click(self, sender, e):
-		header = "Num Cubed:" + "\t\t" + "Cube Root:"
+		header = "Number:" + "\t\t" + "Cube Root:" + "\t\t" + "Num Cubed:"
 		self._listBox1.Items.Add(header)
 		for num in range(-25,26):
 			nc 	= num**3
-			ncr = num**(1.0/3)
-			line = nc + "\t\t" + (ncr)
+			ncr = num**(1/3)
+			line = str(num) + "\t\t" + str(ncr) + "\t\t" + str(nc)
 			self._listBox1.Items.Add(line)
 			# Fix !!!!
 
