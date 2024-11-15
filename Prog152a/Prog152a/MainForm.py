c8c4187385273cc6c8c2871068a39007b3376419
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2024-11-15 15:26:34 -0600

    Update 11/15

---
+++ Prog152a/Prog152a/MainForm.py
@@ -0,0 +1,91 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# listBox1
+		# 
+		self._listBox1.BackColor = System.Drawing.Color.DarkSeaGreen
+		self._listBox1.Font = System.Drawing.Font("Bauhaus 93", 14.25, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.ItemHeight = 21
+		self._listBox1.Location = System.Drawing.Point(12, 12)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(605, 382)
+		self._listBox1.TabIndex = 0
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.SeaGreen
+		self._button1.Font = System.Drawing.Font("Bauhaus 93", 14.25, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(12, 421)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(135, 44)
+		self._button1.TabIndex = 1
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.SeaGreen
+		self._button2.Font = System.Drawing.Font("Bauhaus 93", 14.25, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(247, 421)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(135, 44)
+		self._button2.TabIndex = 2
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.SeaGreen
+		self._button3.Font = System.Drawing.Font("Bauhaus 93", 14.25, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(482, 421)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(135, 44)
+		self._button3.TabIndex = 3
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.Teal
+		self.ClientSize = System.Drawing.Size(629, 468)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._listBox1)
+		self.Name = "MainForm"
+		self.Text = "Prog152a"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		header = "Sum of every multiple of 3 from 3 to 9669:"
+		self._listBox1.Items.Add(header)
+		for num in range(1,9670):
+			result = num * 3
+			# To be completed!!!!
+		
+		self._listBox1.Items.Add(str(sum))
+
+	def Button2Click(self, sender, e):
+		self._listBox1.Items.Clear()
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
