06d932e0e20c6c20071fecca1a558d7303f57821
Ethanbahr <bahr.e@stu.janesville.k12.wi.us>
2024-12-05 15:27:19 -0600

    Update 12/5

---
+++ Pg119/Pg119/MainForm.py
@@ -0,0 +1,159 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self._label1 = System.Windows.Forms.Label()
+		self._label2 = System.Windows.Forms.Label()
+		self._label3 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._textBox2 = System.Windows.Forms.TextBox()
+		self._label4 = System.Windows.Forms.Label()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.DimGray
+		self._button1.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.ForeColor = System.Drawing.Color.Silver
+		self._button1.Location = System.Drawing.Point(12, 130)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(82, 37)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.Gray
+		self._button2.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.ForeColor = System.Drawing.Color.LightGray
+		self._button2.Location = System.Drawing.Point(100, 130)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(82, 37)
+		self._button2.TabIndex = 1
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.SystemColors.ButtonShadow
+		self._button3.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.ForeColor = System.Drawing.Color.Gainsboro
+		self._button3.Location = System.Drawing.Point(188, 130)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(82, 37)
+		self._button3.TabIndex = 2
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.FromArgb(64, 64, 64)
+		self._label1.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.ForeColor = System.Drawing.SystemColors.ControlDark
+		self._label1.Location = System.Drawing.Point(12, 9)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(100, 23)
+		self._label1.TabIndex = 3
+		self._label1.Text = "First name:"
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.SystemColors.WindowFrame
+		self._label2.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.ForeColor = System.Drawing.SystemColors.ActiveBorder
+		self._label2.Location = System.Drawing.Point(12, 43)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(100, 23)
+		self._label2.TabIndex = 4
+		self._label2.Text = "Last name:"
+		self._label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# label3
+		# 
+		self._label3.BackColor = System.Drawing.SystemColors.GrayText
+		self._label3.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label3.ForeColor = System.Drawing.SystemColors.AppWorkspace
+		self._label3.Location = System.Drawing.Point(12, 76)
+		self._label3.Name = "label3"
+		self._label3.Size = System.Drawing.Size(100, 23)
+		self._label3.TabIndex = 5
+		self._label3.Text = "Full name:"
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# textBox1
+		# 
+		self._textBox1.BackColor = System.Drawing.SystemColors.ScrollBar
+		self._textBox1.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox1.ForeColor = System.Drawing.Color.Black
+		self._textBox1.Location = System.Drawing.Point(141, 9)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(129, 26)
+		self._textBox1.TabIndex = 6
+		# 
+		# textBox2
+		# 
+		self._textBox2.BackColor = System.Drawing.Color.GhostWhite
+		self._textBox2.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox2.Location = System.Drawing.Point(141, 43)
+		self._textBox2.Name = "textBox2"
+		self._textBox2.Size = System.Drawing.Size(129, 26)
+		self._textBox2.TabIndex = 7
+		# 
+		# label4
+		# 
+		self._label4.BackColor = System.Drawing.Color.White
+		self._label4.Font = System.Drawing.Font("Times New Roman", 12, System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, 0)
+		self._label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+		self._label4.Location = System.Drawing.Point(141, 76)
+		self._label4.Name = "label4"
+		self._label4.Size = System.Drawing.Size(129, 51)
+		self._label4.TabIndex = 8
+		self._label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.Black
+		self.ClientSize = System.Drawing.Size(285, 174)
+		self.Controls.Add(self._label4)
+		self.Controls.Add(self._textBox2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label3)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "Pg119"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+
+
+	def Button1Click(self, sender, e):
+		fn  = self._textBox1.Text
+		ln  = self._textBox2.Text
+		fun = fn + " " + ln
+		self._label4.Text 	= fun
+
+	def Button2Click(self, sender, e):
+		self._textBox1.Text = ""
+		self._textBox2.Text = ""
+		self._label4.Text 	= ""
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
